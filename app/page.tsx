64d871f74b9dd1b28c93446d37b4316a004b35ef
Ovy Evbodi <evbodiovo@gmail.com>
2024-03-08 22:52:58 +0100

    Add assets

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="font-inter hero_page min-h-screen">
       <div className="hero_wrap bg-red-300 py-[2rem] min-h-screen flex flex-col w-[90vw] lg:w-[76vw] mx-auto justify-between lg:flex-row">
         <div className="bg-blue-300 p-4 h-[45vh] lg:h-[100vh] lg:w-[45vw]">
-          left
+          <div>left</div>
         </div>
         <div className="bg-green-300 p-4 h-[45vh] lg:h-[100vh] lg:w-[45vw]">
           right
