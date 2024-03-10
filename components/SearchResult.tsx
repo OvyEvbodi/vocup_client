1083bbea0ac7e7f894513738a954c21c95ca05c9
Ovy Evbodi <evbodiovo@gmail.com>
2024-03-10 17:18:50 +0100

    Fix bug for searches lacking audios

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -22,7 +22,9 @@ const searchResult: NextPage<SearchResultProps> = (word) => {
         <br/>
         { data && <div className="max-w-full">phonetic: {data.phonetic}</div> }
         <br/>
-        { data && <div className="result_field">audio: {data.phonetics[0].audio}</div> }
+        <div> audio: 
+          { data && data.phonetics.length > 0 && <span> {data.phonetics.length > 1 ? data.phonetics[1].audio : data.phonetics[0].audio}</span> }
+        </div>
         <br/>
         { data && 
           <div>{data.meanings.map((meaning:any, index:any) => 
