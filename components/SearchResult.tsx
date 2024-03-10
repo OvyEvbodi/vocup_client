d0dd3d55752423820c9187b08dbee6f0dbe44e27
Ovy Evbodi <evbodiovo@gmail.com>
2024-03-10 16:30:21 +0100

    Update search result format

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -6,20 +6,39 @@ export interface SearchResultProps {
 
 const searchResult: NextPage<SearchResultProps> = (word) => {
     const data = word.data[0];
-    return (
-        <div className="max-w-[100vw]">
-            {data && <div>name: {data.word}</div> }
-            <br/>
-            { data && <div>phonetic: {data.phonetic}</div> }
-            <br/>
-            { data && <div>audio: {data.phonetics[0].audio}</div> }
-            <br/>
-            { data && <div>{data.meanings.map((meaning:any, index:any) => <div><div>meaning({index +1}): {JSON.stringify(meaning)}</div><br /></div>)}</div>}
-            <br/>
-            <div>origin</div>
-            {/* {word && JSON.stringify(word)} */}
-
+    const FormatMeanings = (meaning:any) => {
+      meaning.definitions.map((item:any) => (item) )
+      return (
+        <div>
+          <div>Part of speech: {meaning.partOfSpeech}</div>
+          <div>Definition: {JSON.stringify(meaning.definitions)}</div>
         </div>
+      )
+      };
+
+    return (
+      <div className="max-w-[100vw]">
+        {data && <div>name: {data.word}</div> }
+        <br/>
+        { data && <div className="max-w-full">phonetic: {data.phonetic}</div> }
+        <br/>
+        { data && <div className="result_field">audio: {data.phonetics[0].audio}</div> }
+        <br/>
+        { data && 
+          <div>{data.meanings.map((meaning:any, index:any) => 
+            <div key={index}>
+              <div>
+                meaning({index +1}): <br />
+                Part of speech: {meaning.partOfSpeech} <br />
+                Definition: {meaning.definitions[0].definition} <br />
+                Example: {meaning.definitions[0].example} 
+              </div>
+              <br />
+            </div>
+        )}</div> }
+        <br/>
+        {/* {word && JSON.stringify(word)} */}
+      </div>
     )
 };
 
