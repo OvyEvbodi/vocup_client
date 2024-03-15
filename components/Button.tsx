703e2998f2f1eb0f5939d49689bd8e4d886302ea
Ovy Evbodi <evbodiovo@gmail.com>
2024-03-15 19:21:21 +0100

    Update Button component style

--- components/Button.tsx
+++ components/Button.tsx
@@ -14,7 +14,7 @@ export interface ButtonProps {
 // button component 
 const Button: NextPage<ButtonProps> = ({ type, text, onClick, disabled }) => {
     return(
-    <button onClick={ onClick } type={ type } disabled={ disabled } className='py-1 capitalize px-4 text-sm text-slate-900 rounded border border-slate-900 hover:bg-dark_bg hover:text-slate-50 transition-all duration-300'>{ text }</button>
+    <button onClick={ onClick } type={ type } disabled={ disabled } className='py-1 capitalize px-4 text-sm text-slate-900 rounded border border-slate-900 hover:bg-grey hover:text-light_text transition-all-duration-600'>{ text }</button>
     )
 };
 
