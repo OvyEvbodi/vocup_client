3926ee1d52b24543021e7090855dbf2dd942ae96
Ovy Evbodi <evbodiovo@gmail.com>
2024-05-08 13:16:03 +0100

    Redesign profile page

--- app/profile/components/ProfileSection.tsx
+++ app/profile/components/ProfileSection.tsx
@@ -3,15 +3,19 @@
 // import user context
 import { UserState } from '@/contexts/UserContext'
 import Stats from '@/app/profile/components/Stats'
+import WordLookUp from '@/components/WordLookUp'
 
 const ProfileSection = ( user: UserState ) => {
 
   
   return (
     <>
-      <section>
-        <h1>Welcome back! { user.username } </h1>
+      <section className='bg-blue p-2 text-center'>
+        <h1 className='font-bold text-[1.2rem] capitalize p-2'>Welcome back! { user.username } </h1>
         <p>We&apos;re working on a cool stats page for you to track your learning progress!</p>
+      </section>
+      <section>
+        <WordLookUp />
       </section>
       <section>
         <Stats { ...user}/>
